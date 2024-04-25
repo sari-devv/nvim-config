cb1b6d53404cfd9eccb0de4bd852fe924b37dbb7
bulldog9800 <abed.sari4@gmail.com>
2024-04-25 14:23:05 +0300

    Add live_grep with args
    Signed-off-by: bulldog9800 <abed.sari4@gmail.com>

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,8 @@
 require("telescope").setup()
+require("telescope").load_extension("live_grep_args")
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fg', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- lua/sari/plugins/telescope.lua
+++ lua/sari/plugins/telescope.lua
@@ -1,4 +1,11 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim'}
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+            'nvim-telescope/telescope-live-grep-args.nvim',
+            version = "^1.0.0"
+        }
+    }
 }
