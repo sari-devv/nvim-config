5b0c12aa194005c36373301ea08c6ba2cd0494ef
whysoserious <abed.sari4@gmail.com>
2024-04-26 14:37:11 +0300

    setup navic

--- lua/sari/plugins/statusline.lua
+++ lua/sari/plugins/statusline.lua
@@ -1,7 +1,28 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        {
+            'Smiteshp/nvim-navic',
+            config = function()
+                require("nvim-navic").setup {
+                    lsp = {
+                        auto_attach = true
+                    }
+                }
+            end
+        }
+    },
     config = function()
-        require('lualine').setup()
+        require('lualine').setup {
+            winbar = {
+                lualine_a = {
+                    function()
+                        return require("nvim-navic").get_location()
+                    end
+                }
+            }
+        }
     end
+
 }
