8b5dae858af26f9b6c78a7bcaa51579154850b53
bulldog9800 <abed.sari4@gmail.com>
2024-05-29 16:38:24 +0300

    Add TS text objects
    Signed-off-by: bulldog9800 <abed.sari4@gmail.com>

--- lua/sari/plugins/treesitter.lua
+++ lua/sari/plugins/treesitter.lua
@@ -1,6 +1,12 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,	
+    {"nvim-treesitter/nvim-treesitter",
+        build = function()
+            require("nvim-treesitter.install").update({ with_sync = true })()
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        event = "VeryLazy",
+        enabled = true,
+    }
 }
