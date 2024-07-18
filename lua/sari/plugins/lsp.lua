6fd919dde83851d53442e10a55ac9edeb3e52fde
bulldog9800 <abed.sari4@gmail.com>
2024-07-18 15:20:39 +0300

    Add type checker lsp
    Signed-off-by: bulldog9800 <abed.sari4@gmail.com>

--- lua/sari/plugins/lsp.lua
+++ lua/sari/plugins/lsp.lua
@@ -215,6 +215,20 @@ return {
                         },
                     },
                 },
+                typos_lsp = {
+                    -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+                    cmd_env = { RUST_LOG = "error" },
+                    init_options = {
+                        -- Custom config. Used together with a config file found in the workspace or its parents,
+                        -- taking precedence for settings declared in both.
+                        -- Equivalent to the typos `--config` cli argument.
+                        config = '~/code/typos-lsp/crates/typos-lsp/tests/typos.toml',
+                        -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
+                        -- Defaults to error.
+                        diagnosticSeverity = "Error"
+                    }
+
+                }
             }
 
             -- Ensure the servers and tools above are installed
